c087b7eb65c2b7cfa3829f5bbf792f271a171654
krapnik <krapnik@qq.com>
2019-11-12 00:57:25 +0800

    兼容plist文件

--- app.js
+++ app.js
@@ -1,69 +1,133 @@
-const fs = require('fs'), gm = require('gm').subClass({imageMagick: true});
-
+const fs = require('fs'),
+    plist = require('plist'),
+    gm = require('gm').subClass({ imageMagick: true });
 
 const file = process.argv.splice(2).toString();
 
-let json, frames, frame, img_name, output_name, coordinate_x, coordinate_y, img_width, img_height, buf, img_arr = [],
+let frames, textureRect, frame, img_name, output_name, coordinate_x, coordinate_y, img_width, img_height, rotated, buf, img_arr = [],
     splitIdx = 0, maxSplitIdx;
 if (!file) {
     console.error('缺少命令行参数！');
     return;
 }
+if (file.indexOf(".plist") > -1) {
+    getSpritePlist(file);
+} else if (file.indexOf(".json") > -1) {
+    getSpriteJson(file);
+} else {
+    console.log('暂不支持该文件格式！');
+};
 
-function getSpriteJson(path) {
-    fs.readFile(path, function (err, data) {
-        if (err) {
-            console.error(err);
-            return;
+async function getSpritePlist(path) {
+    let json = plist.parse(fs.readFileSync(file, 'utf8'));
+    if (json && json['frames'] && json['metadata']) {
+        frames = json['frames'];
+        img_name = json['metadata']["textureFileName"];
+    } else {
+        console.error('plist文件结构不符合要求！');
+        return;
+    }
+    let version = json['metadata']['format'];
+    for (let item in frames) {
+        frame = frames[item];
+        output_name = item.replace(".png", "");
+        switch (version) {
+            case 1:
+                textureRect = transStr(frame['frame']);
+                rotated = false;
+                break;
+            case 2:
+                textureRect = transStr(frame['frame']);
+                rotated = frame['rotated'];
+                break;
+            case 3:
+                textureRect = transStr(frame['textureRect']);
+                rotated = frame['textureRotated'];
+                break;
+            default:
+                textureRect = transStr(frame['textureRect']);
+                break;
         }
-        json = JSON.parse(data.toString());
-        if (json && json['frames'] && json['file']) {
-            frames = json['frames'];
-            img_name = json['file'];
-        } else {
-            console.error('json文件结构不符合要求！');
-            return;
-        }
+        coordinate_x = textureRect[0];
+        coordinate_y = textureRect[1];
+        img_width = rotated ? textureRect[3] : textureRect[2];
+        img_height = rotated ? textureRect[2] : textureRect[3];
 
-        // console.log(json);
-        for (let item in frames) {
-            frame = frames[item]
-            output_name = item;
-            coordinate_x = frame['x'];
-            coordinate_y = frame['y'];
-            img_width = frame['w'];
-            img_height = frame['h'];
-            img_arr.push({
-                'img_name': img_name,
-                'output_name':output_name,
-                "coordinate_x": coordinate_x,
-                "coordinate_y": coordinate_y,
-                "img_width": img_width,
-                "img_height": img_height
-            });
-        }
-        maxSplitIdx = img_arr.length;
-        spriteSpliter(img_arr[splitIdx].img_name, img_arr[splitIdx].output_name, img_arr[splitIdx].img_width, img_arr[splitIdx].img_height, img_arr[splitIdx].coordinate_x, img_arr[splitIdx].coordinate_y);
-    })
+        img_arr.push({
+            'img_name': img_name,
+            'output_name': output_name,
+            "coordinate_x": coordinate_x,
+            "coordinate_y": coordinate_y,
+            "img_width": img_width,
+            "img_height": img_height,
+            "rotated": rotated,
+        });
+    }
+    maxSplitIdx = img_arr.length;
+    for (let i = 0; i < maxSplitIdx; i++) {
+        let img = img_arr[i];
+        await spriteSpliter(img.img_name, img.output_name, img.img_width, img.img_height, img.coordinate_x, img.coordinate_y, img.rotated);
+    }
+
+};
+
+async function getSpriteJson(path) {
+    let json = JSON.parse(fs.readFileSync(file, 'utf8'));
+    if (json && json['frames'] && json['file']) {
+        frames = json['frames'];
+        img_name = json['file'];
+    } else {
+        console.error('json文件结构不符合要求！');
+        return;
+    }
+
+    for (let item in frames) {
+        frame = frames[item]
+        output_name = item;
+        coordinate_x = frame['x'];
+        coordinate_y = frame['y'];
+        img_width = frame['w'];
+        img_height = frame['h'];
+        rotated = frames['rotated'];
+        img_arr.push({
+            'img_name': img_name,
+            'output_name': output_name,
+            "coordinate_x": coordinate_x,
+            "coordinate_y": coordinate_y,
+            "img_width": img_width,
+            "img_height": img_height,
+            "rotated": rotated,
+        });
+    }
+    maxSplitIdx = img_arr.length;
+    for (let i = 0; i < maxSplitIdx; i++) {
+        let img = img_arr[i];
+        await spriteSpliter(img.img_name, img.output_name, img.img_width, img.img_height, img.coordinate_x, img.coordinate_y, img.rotated);
+    }
 }
 
-getSpriteJson(file);
+function transStr(string) {//"{{41,52},{11,19}}"=>["41","52","11","19"]
+    return string.replace(/{|}/g, "").split(",");
+}
 
-function spriteSpliter(img_name, output_name, width, height, x, y) {
+async function spriteSpliter(img_name, output_name, width, height, x, y, rotated) {
     buf = gm(`./src/${img_name}`)
         .crop(width, height, x, y);
+    if (rotated) {
+        buf = buf.rotate("transparent", -90);
+    }
     buf.write(`./output/${output_name}.png`, function (err) {
         if (!err) {
-            console.log(`${splitIdx+1}/${maxSplitIdx}=>${output_name}.png`);
             splitIdx++;
-            if (splitIdx < maxSplitIdx) {
-                spriteSpliter(img_arr[splitIdx].img_name, img_arr[splitIdx].output_name, img_arr[splitIdx].img_width, img_arr[splitIdx].img_height, img_arr[splitIdx].coordinate_x, img_arr[splitIdx].coordinate_y);
+            console.log(`${splitIdx}/${maxSplitIdx}=>${output_name}.png`);
+            if (splitIdx == maxSplitIdx) {
+                console.log("文件裁剪完成！");
             }
         } else {
             console.log(err.message || "出错了！");
         }
     });
-}
+};
 
 
 
